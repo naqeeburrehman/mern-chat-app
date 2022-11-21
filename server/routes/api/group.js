01ab3cff942a58c28664df99afa774845e3eefb7
naqeeburrehman <naqeeburrehman.pk@gmail.com>
2022-11-21 18:51:02 +0000

    added groups controllers

--- server/routes/api/group.js
+++ server/routes/api/group.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const groupController = require("../../controllers/groupController");
 
 router.route("/")
-.post(groupController.createGroupChat)
-.patch(groupController.renameGroup)
-.put(groupController.addToGroup)
-.delete(groupController.removeFromGroup)
+    .post(groupController.createGroupChat);
+
+router
+    .route("/:id")
+    .patch(groupController.updateGroup)
+    .put(groupController.addToGroup)
+    .delete(groupController.removeFromGroup);
 
 module.exports = router;
