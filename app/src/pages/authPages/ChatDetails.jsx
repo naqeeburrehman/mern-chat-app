ce04a537a18094af0ad5b573911e4f1caf4d2e55
naqeeburrehman <naqeeburrehman.pk@gmail.com>
2023-01-18 17:15:34 +0000

    code update

--- app/src/pages/authPages/ChatDetails.jsx
+++ app/src/pages/authPages/ChatDetails.jsx
@@ -1,7 +1,9 @@
-import { ExclamationCircleIcon, UserMinusIcon } from "@heroicons/react/24/outline";
+import { ExclamationCircleIcon, MagnifyingGlassIcon, UserMinusIcon, UserPlusIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import { useGetChatsQuery } from "../../features/chat/chatApiSlice";
+import { useGetSearchsQuery } from "../../features/search/searchApiSlice";
 import useAuth from "../../hooks/useAuth";
 
 const ChatDetails = () => {
@@ -71,7 +73,160 @@ const GroupDetails = ({ data }) => {
     return (
         <div>
             <span>Group Details</span>
+            {data.groupAdmin?._id === id ? <Search /> : null}
             <div className="max-h-60 overflow-scroll rounded-2xl m-2 p-1 flex flex-wrap">{users}</div>
+        </div>
+    );
+};
+
+const Search = ({}) => {
+    const onAddUser = () => {
+        console.log("asgasgsad");
+    };
+
+    const [skip, setSkip] = useState(true);
+    const [searchQuery, setSearchQuery] = useState({ u: "", q: 24 });
+    const {
+        data: contacts,
+        isFetching,
+        isSuccess,
+        isError,
+        error,
+    } = useGetSearchsQuery(
+        { u: searchQuery.u, q: searchQuery.q },
+        {
+            skip,
+            refetchOnMountOrArgChange: true,
+        }
+    );
+    const onSearch = (e, searchValue) => {
+        e.preventDefault();
+        if (searchValue) {
+            setSearchQuery({ u: searchValue });
+            setSkip(false);
+        }
+    };
+    let contactlist;
+    if (isError) {
+        contactlist = <IfNotSuccess message={error} />;
+    }
+    if (isSuccess && contacts) {
+        if (contacts.ids.length > 0) {
+            const { entities } = contacts;
+            let contactCards = [];
+            Object.keys(entities).forEach((key, index) => {
+                contactCards.push(
+                    <div key={entities[key]._id} className="w-full sm:w-1/2 md:w-1/3 lg:w-3/12 p-1">
+                        <div className="rounded-xl px-3 py-2 flex justify-between bg-secondary-100 items-center">
+                            <div className="flex items-center">
+                                <img className="w-12 h-12 rounded-full" src={entities[key].img} alt="profile image" />
+                                <div className="pl-4 flex flex-col">
+                                    <span className="text-ellipsis overflow-hidden whitespace-nowrap sm:w-40 md:w-28 xl:w-40 text-secondary-600 text-lg font-semibold">
+                                        {entities[key].name}
+                                    </span>
+                                    <span className="text-primary-600 text-sm">{entities[key].phone}</span>
+                                </div>
+                            </div>
+                            <div className="flex justify-center items-center">
+                                <button
+                                    type="button"
+                                    onClick={() => onAddUser(entities[key])}
+                                    className="bg-primary-600 hover:bg-primary-500 text-secondary-100 rounded p-2 "
+                                >
+                                    <UserPlusIcon className="w-6 h-6" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                );
+            });
+            contactlist = <div className="h-40 overflow-scroll px-1 py-2 flex flex-wrap">{contactCards}</div>;
+        } else contactlist = <IfNotSuccess message={"No Contacts Found"} />;
+    }
+    if (isFetching) {
+        contactlist = <Skeleton />;
+    }
+    return (
+        <>
+            <SearchBar onSearch={onSearch} />
+            {contactlist}
+        </>
+    );
+};
+
+const SearchBar = ({ onSearch }) => {
+    const [searchValue, setSearchValue] = useState("");
+    return (
+        <form
+            onSubmit={(e) => {
+                onSearch(e, searchValue);
+                setSearchValue("");
+            }}
+            className="flex bg-secondary-100 text-secondary-100 p-2"
+        >
+            <input
+                className="text-secondary-700 bg-secondary-300 focus:bg-secondary-200 hover:bg-secondary-200 w-full mr-1 px-4 py-2 rounded"
+                type="text"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <button className="p-2 bg-primary-600 rounded hover:bg-primary-500" type="submit">
+                <MagnifyingGlassIcon className="w-6 h-6" />
+            </button>
+        </form>
+    );
+};
+
+const IfNotSuccess = ({ message }) => {
+    return (
+        <div className="pt-24 md:pt-40 flex flex-col justify-center items-center">
+            <ExclamationCircleIcon className="w-40 h-40 text-secondary-200" />
+            <span className="pt-10 text-secondary-600 font-semibold text-xl">{message}</span>
+        </div>
+    );
+};
+
+const Skeleton = () => {
+    return (
+        <div
+            role="status"
+            className="p-4 space-y-4 rounded border border-secondary-200 divide-y divide-secondary-200 shadow animate-pulse "
+        >
+            <div className="flex justify-between items-center">
+                <div>
+                    <div className="h-2.5 bg-secondary-300 rounded-full w-24 mb-2.5"></div>
+                    <div className="w-32 h-2 bg-secondary-200 rounded-full"></div>
+                </div>
+                <div className="h-2.5 bg-secondary-300 rounded-full w-12"></div>
+            </div>
+            <div className="flex justify-between items-center pt-4">
+                <div>
+                    <div className="h-2.5 bg-secondary-300 rounded-full w-24 mb-2.5"></div>
+                    <div className="w-32 h-2 bg-secondary-200 rounded-full"></div>
+                </div>
+                <div className="h-2.5 bg-secondary-300 rounded-full w-12"></div>
+            </div>
+            <div className="flex justify-between items-center pt-4">
+                <div>
+                    <div className="h-2.5 bg-secondary-300 rounded-full w-24 mb-2.5"></div>
+                    <div className="w-32 h-2 bg-secondary-200 rounded-full"></div>
+                </div>
+                <div className="h-2.5 bg-secondary-300 rounded-full w-12"></div>
+            </div>
+            <div className="flex justify-between items-center pt-4">
+                <div>
+                    <div className="h-2.5 bg-secondary-300 rounded-full w-24 mb-2.5"></div>
+                    <div className="w-32 h-2 bg-secondary-200 rounded-full"></div>
+                </div>
+                <div className="h-2.5 bg-secondary-300 rounded-full w-12"></div>
+            </div>
+            <div className="flex justify-between items-center pt-4">
+                <div>
+                    <div className="h-2.5 bg-secondary-300 rounded-full w-24 mb-2.5"></div>
+                    <div className="w-32 h-2 bg-secondary-200 rounded-full"></div>
+                </div>
+                <div className="h-2.5 bg-secondary-300 rounded-full w-12"></div>
+            </div>
         </div>
     );
 };
